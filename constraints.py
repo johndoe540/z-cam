79126e1e65c8482f0ff21e63115ebcd5c0bee91a
johndoe540 <130331032+johndoe540@users.noreply.github.com>
2025-10-25 12:16:03 +0800

    Update constraints.py

--- constraints.py
+++ constraints.py
@@ -1,3 +1,3 @@
 API_KEY = "xxxxxx:xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-BITLY_ACCESS_TOKEN = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+BITLY_ACCESS_TOKEN = "f6e7c854d14e78c6bc36be0f15eca44b6fea7b7d"
 ngrok_auth_token = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
